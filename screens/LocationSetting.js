6494128cabb4437f5208316a5600180e61071bf6
Khalid <khalidahmed333@hotmail.com>
2018-01-16 07:53:17 +0200

    Adds a cover image in location setting screen

--- screens/LocationSetting.js
+++ screens/LocationSetting.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Constants } from 'expo';
-import { View, AsyncStorage } from 'react-native';
+import { View, AsyncStorage, Image } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import Colors from '../constants/Colors';
 import LoadingIndicator from '../components/LoadingIndicator';
 import { NavigationActions } from 'react-navigation';
+import { Dimensions } from 'react-native';
 
 export default class Header extends React.Component {
     static navigationOptions = {
@@ -43,65 +44,74 @@ export default class Header extends React.Component {
             return (    <LoadingIndicator size="large" />   );
 
         return (
-            <View style={{ flex: 1 }}>
-                <GooglePlacesAutocomplete
-                  placeholder="ابحث عن مكان التوصيل"
-                  minLength={2}
-                  autoFocus={false}
-                  returnKeyType={'search'}
-                  listViewDisplayed="auto"
-                  fetchDetails={false}
-                  renderDescription={row => row.description}
-                  onPress={(data, details = null) => {
-                    // 'details' is provided when fetchDetails = true
-                    //console.log(data.description);
-                    //console.log(details);
-                    AsyncStorage.setItem('location', data.description).then(() => {
-                        this.props.navigation.navigate("Main", { });
-                    });
-                  }}
-                  getDefaultValue={() => {
-                    return ''; // text input default value
-                  }}
-                  query={{
-                    key: 'AIzaSyDS5yK4xbCDrLVcF-88zwcKRPnSoPxdKIc',
-                    language: 'ar', // language of the results
-                  }}
-                  styles={{
-                    description: {
-                        fontWeight: 'bold',
-                        color: '#444444',
-                    },
-                    predefinedPlacesDescription: {
-                      color: Colors.mainColor,
-                    },
-                    textInputContainer: {
-                        backgroundColor: 'white',
-                        borderBottomColor: '#F2F3F5',
-                        height: 60
-                    },
-                    textInput: {
-                        borderRadius: 13,
-                        backgroundColor: '#F2F3F5',
-                        height: 45
-                    },
-                    container: {
-                        backgroundColor: 'white'
-                    }
-                  }}
-                  currentLocation={false}
-                  currentLocationLabel="مكاني الحالي"
-                  nearbyPlacesAPI="GooglePlacesSearch"
-                  GoogleReverseGeocodingQuery={{
-                    // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-                  }}
-                  GooglePlacesSearchQuery={{
-                    // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                    rankby: 'distance',
-                    radius: 50000
-                  }}
-                  debounce={200}
-                />
+            <View style={{ flex:1}}>
+                <View style = {{ flex: 0.5, backgroundColor: 'white', marginTop:-2 }} >
+                    <Image
+                        style={ {width: Dimensions.get('window').width, height: '100%', margin:0}}
+                        resizeMode='contain'
+                        source={require('../assets/images/location-picker-cover.jpeg')}/>
+                </View>
+
+                <View style={{ flex: 1 }}>
+                    <GooglePlacesAutocomplete
+                      placeholder="ابحث عن مكان التوصيل"
+                      minLength={2}
+                      autoFocus={false}
+                      returnKeyType={'search'}
+                      listViewDisplayed="auto"
+                      fetchDetails={false}
+                      renderDescription={row => row.description}
+                      onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        //console.log(data.description);
+                        //console.log(details);
+                        AsyncStorage.setItem('location', data.description).then(() => {
+                            this.props.navigation.navigate("Main", { });
+                        });
+                      }}
+                      getDefaultValue={() => {
+                        return ''; // text input default value
+                      }}
+                      query={{
+                        key: 'AIzaSyDS5yK4xbCDrLVcF-88zwcKRPnSoPxdKIc',
+                        language: 'ar', // language of the results
+                      }}
+                      styles={{
+                        description: {
+                            fontWeight: 'bold',
+                            color: '#444444',
+                        },
+                        predefinedPlacesDescription: {
+                          color: Colors.mainColor,
+                        },
+                        textInputContainer: {
+                            backgroundColor: 'white',
+                            borderBottomColor: '#F2F3F5',
+                            height: 60
+                        },
+                        textInput: {
+                            borderRadius: 13,
+                            backgroundColor: '#F2F3F5',
+                            height: 45
+                        },
+                        container: {
+                            backgroundColor: 'white'
+                        }
+                      }}
+                      currentLocation={false}
+                      currentLocationLabel="مكاني الحالي"
+                      nearbyPlacesAPI="GooglePlacesSearch"
+                      GoogleReverseGeocodingQuery={{
+                        // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+                      }}
+                      GooglePlacesSearchQuery={{
+                        // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                        rankby: 'distance',
+                        radius: 50000
+                      }}
+                      debounce={200}
+                    />
+                </View>
             </View>
         );
     }
