2402cc9736404ed05ce8fd5ee5ef218e730fe763
Khalid <khalidahmed333@hotmail.com>
2018-01-24 07:19:44 +0200

    Fixes RootNavigation.js

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -13,13 +13,12 @@ import ResetPassword from '../screens/ResetPassword';
 
 const RootStackNavigator = StackNavigator(
     {
-      Main: { screen: MainTabNavigator },
-        ResetPassword: { screen: ResetPassword },
         LocationSetting: { screen: LocationSetting },
         Signin: { screen: Signin },
         Signup: { screen: Signup },
         CodeVerification: { screen: CodeVerification },
-
+        ResetPassword: { screen: ResetPassword },
+        Main: { screen: MainTabNavigator },
         Restaurant: {screen : Restaurant}
     },
     {
