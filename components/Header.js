36e0d78c885c136e9486702585934349ddf3825e
Hossam Samir <Hossameos1@gmail.com>
2018-02-17 18:27:07 +0200

    Added sort icon in the header of home screen, close #35

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
-  Platform,
-  View,
-  TextInput,
-  StyleSheet,
-  Text,
-  AsyncStorage,
-  TouchableOpacity
+	Platform,
+	View,
+	TextInput,
+	StyleSheet,
+	Text,
+	AsyncStorage,
+	TouchableOpacity
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
@@ -14,144 +14,164 @@ import { Font } from 'expo';
 
 const fontCol = 'white';
 const iconCol = 'rgba(255,255,255,0.8)';
-const bgCol   = Colors.mainColor;
+const bgCol = Colors.mainColor;
 
 export default class Header extends React.Component {
-    componentDidMount() {
+	componentDidMount() {
 		this.setState({ fontLoaded: '1' });
 
-        AsyncStorage.getItem('location').then(
-            (value) => {
-                this.setState({
-                    location: (value === null) ? 'لم يتم تحديد مكان' : value
-                });
-            });
+		AsyncStorage.getItem('location').then(value => {
+			this.setState({
+				location: value === null ? 'لم يتم تحديد مكان' : value
+			});
+		});
 	}
 
-    constructor(props){
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            displaySearch: 0,
-            searchText: '',
-            fontLoaded: '0',
-            location: ' '
-        }
-    }
+		this.state = {
+			displaySearch: 0,
+			searchText: '',
+			fontLoaded: '0',
+			location: ' '
+		};
+	}
 
-    doSearch = () => {
-        if(this.state.searchText.length)
-            this.props.navigation.navigate("SearchResult", {searchingFor: this.state.searchText});
-    };
+	doSearch = () => {
+		if (this.state.searchText.length)
+			this.props.navigation.navigate('SearchResult', {
+				searchingFor: this.state.searchText
+			});
+	};
 
-    trimName = (str) => {
-        return (str.length > 35) ? (str.substring(0, 32) + "...") : str;
-    };
+	trimName = str => {
+		return str.length > 35 ? str.substring(0, 32) + '...' : str;
+	};
 
-    renderHeaderButton = () => {
-        if(this.state.displaySearch)
-        {
-            return (
-                <TouchableOpacity onPress={ () => this.setState({displaySearch: 0}) }>
-                    <Ionicons
-                        name={ (Platform.OS === 'ios') ? "ios-arrow-up" : "md-arrow-dropup" }
-                        size={32}
-                        style={{ padding: 10 }}
-                        color={iconCol}
-                    />
-                </TouchableOpacity>
-            );
-        }
-        else
-        {
-            return (
-                <TouchableOpacity onPress={ () => this.setState({displaySearch: 1}) }>
-                    <Ionicons
-                        name={ (Platform.OS === 'ios') ? "ios-search" : "md-search" }
-                        size={32}
-                        style={{ padding: 10 }}
-                        color={iconCol}
-                    />
-                </TouchableOpacity>
-            );
-        }
-    };
+	renderHeaderButton = () => {
+		if (this.state.displaySearch) {
+			return (
+				<TouchableOpacity onPress={() => this.setState({ displaySearch: 0 })}>
+					<Ionicons
+						name={Platform.OS === 'ios' ? 'ios-arrow-up' : 'md-arrow-dropup'}
+						size={32}
+						style={{ padding: 10 }}
+						color={iconCol}
+					/>
+				</TouchableOpacity>
+			);
+		} else {
+			return (
+				<TouchableOpacity onPress={() => this.setState({ displaySearch: 1 })}>
+					<Ionicons
+						name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'}
+						size={32}
+						style={{ padding: 10 }}
+						color={iconCol}
+					/>
+				</TouchableOpacity>
+			);
+		}
+	};
 
-    shouldDisplaySearchBar = () => {
-        if(this.state.displaySearch)
-        {
-            return (
-                <View style={styles.box}>
-                    <TouchableOpacity onPress={ () => this.doSearch() }>
-                        <Ionicons
-                            name={ (Platform.OS === 'ios') ? "ios-search" : "md-search" }
-                            size={32}
-                            style={{ padding: 10 }}
-                            color='gray'
-                        />
-                    </TouchableOpacity>
-                    <TextInput
-                        {...this.props}
-                        style={styles.input}
-                        placeholderTextColor='#999999'
-                        placeholder="البحث عن مطعم"
-                        returnKeyType={"search"}
-                        underlineColorAndroid="transparent"
-                        onChangeText={(text) => this.setState({searchText:text})}
-                        onSubmitEditing={(event) => this.doSearch() }
-                    />
-                </View>
-            );
-        }
-        else return null;
-    };
+	shouldDisplaySearchBar = () => {
+		if (this.state.displaySearch) {
+			return (
+				<View style={styles.box}>
+					<TouchableOpacity onPress={() => this.doSearch()}>
+						<Ionicons
+							name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'}
+							size={32}
+							style={{ padding: 10 }}
+							color="gray"
+						/>
+					</TouchableOpacity>
+					<TextInput
+						{...this.props}
+						style={styles.input}
+						placeholderTextColor="#999999"
+						placeholder="البحث عن مطعم"
+						returnKeyType={'search'}
+						underlineColorAndroid="transparent"
+						onChangeText={text => this.setState({ searchText: text })}
+						onSubmitEditing={event => this.doSearch()}
+					/>
+				</View>
+			);
+		} else return null;
+	};
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.topbox}>
-                    <View style={{ flex:0.2, flexDirection: 'row', justifyContent:'flex-start' }}>
-                        {this.renderHeaderButton()}
-                    </View>
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.topbox}>
+					<View
+						style={{
+							flex: 0.2,
+							flexDirection: 'row',
+							justifyContent: 'flex-start'
+						}}
+					>
+						<TouchableOpacity
+							onPress={() => this.props.navigation.navigate('FilterScreen')}
+						>
+							<Ionicons
+								name="ios-funnel"
+								size={29}
+								style={{ padding: 10 }}
+								color={iconCol}
+							/>
+						</TouchableOpacity>
 
-                    <View style={{ flex:1, flexDirection: 'row', justifyContent:'center' }}>
-                        {this.state.fontLoaded == '1' ? (
-                            <Text style={{ color: fontCol, fontSize: 16, fontFamily: 'myfont' }}> {this.trimName(this.state.location)}</Text>
-                        ) : null}
-                    </View>
-                </View>
+						{this.renderHeaderButton()}
+					</View>
 
-                {this.shouldDisplaySearchBar()}
-            </View>
-        );
-    }
+					<View
+						style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}
+					>
+						{this.state.fontLoaded == '1' ? (
+							<Text
+								style={{ color: fontCol, fontSize: 16, fontFamily: 'myfont' }}
+							>
+								{' '}
+								{this.trimName(this.state.location)}
+							</Text>
+						) : null}
+					</View>
+				</View>
+
+				{this.shouldDisplaySearchBar()}
+			</View>
+		);
+	}
 }
 
 var styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white'
-    },
-    topbox: {
-        paddingTop:8,
-        paddingBottom:6,
+	container: {
+		backgroundColor: 'white'
+	},
+	topbox: {
+		paddingTop: 8,
+		paddingBottom: 6,
 		height: 55,
-        flexDirection: 'row',
+		flexDirection: 'row',
 		flexWrap: 'wrap',
 		alignItems: 'center',
-        justifyContent: 'center',
+		justifyContent: 'center',
 		backgroundColor: bgCol,
-        borderBottomColor: '#EEEEEE',
-        borderBottomWidth: 1
+		borderBottomColor: '#EEEEEE',
+		borderBottomWidth: 1
 	},
 	box: {
 		height: 45,
 		backgroundColor: Colors.smoothGray,
-        borderRadius: 9,
+		borderRadius: 9,
 		flexDirection: 'row',
 		flexWrap: 'wrap',
 		alignItems: 'center',
-        marginVertical: 12,
-        marginHorizontal: 10
+		marginVertical: 12,
+		marginHorizontal: 10
 	},
 
 	input: {
@@ -166,5 +186,5 @@ var styles = StyleSheet.create({
 		marginLeft: 7,
 
 		flex: 1
-	},
+	}
 });
