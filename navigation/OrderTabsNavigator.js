5a54c55027a3598ddefb4d20c39f6f6bfa69c6d7
Hossam Samir <Hossameos1@gmail.com>
2018-02-26 09:43:52 +0200

    order tab renaming

--- navigation/OrderTabsNavigator.js
+++ navigation/OrderTabsNavigator.js
@@ -6,36 +6,36 @@ import OrdersHistory from '../screens/orders/OrdersHistory';
 import AdminOrders from '../screens/orders/AdminOrders';
 
 export default TabNavigator(
-  {
-    "الطلبات الحالية": {
-      screen: CurrentOrders,
-    },
-    "الطلبات السابقة": {
-      screen: OrdersHistory,
-    },
-    "طلبات المطعم": {
-      screen: AdminOrders,
-    },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-        header: null,
-    }),
-    tabBarOptions: {
-      showLabel: true,
-      labelStyle: {
-          fontWeight: '300',
-          color: '#ffffff',
-          fontFamily: 'myfont',
-          fontSize: 12
-      },
-      style: {
-          backgroundColor: '#EBB70A',
-      },
-      activeTintColor: '#000',
-  },
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: true,
-  }
+	{
+		'طلباتي الحالية': {
+			screen: CurrentOrders
+		},
+		'طلباتي السابقة': {
+			screen: OrdersHistory
+		},
+		'صاحب المتجر': {
+			screen: AdminOrders
+		}
+	},
+	{
+		navigationOptions: ({ navigation }) => ({
+			header: null
+		}),
+		tabBarOptions: {
+			showLabel: true,
+			labelStyle: {
+				fontWeight: '300',
+				color: '#ffffff',
+				fontFamily: 'myfont',
+				fontSize: 12
+			},
+			style: {
+				backgroundColor: '#EBB70A'
+			},
+			activeTintColor: '#000'
+		},
+		tabBarPosition: 'bottom',
+		animationEnabled: false,
+		swipeEnabled: true
+	}
 );
