a71f661c216748a0190257266b7f5ee4963685ec
Hossam Samir <Hossameos1@gmail.com>
2018-02-27 03:24:36 +0200

    made meal quantity controler

--- screens/SingleMeal.js
+++ screens/SingleMeal.js
@@ -1,170 +1,222 @@
 import React from 'react';
-import { ScrollView,KeyboardAvoidingView, TextInput,StyleSheet,Text,FlatList,TouchableOpacity,View,AsyncStorage } from 'react-native';
+import {
+	ScrollView,
+	KeyboardAvoidingView,
+	TextInput,
+	StyleSheet,
+	Text,
+	FlatList,
+	TouchableOpacity,
+	View,
+	AsyncStorage
+} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import Colors from '../constants/Colors';
 import MealBox from '../components/MealBox';
-import { Button } from "react-native-elements";
+import { Button } from 'react-native-elements';
 import RestaurantBox from '../components/RestaurantBox';
 import Server from '../constants/server';
 import LoadingIndicator from '../components/LoadingIndicator';
 // import { NavigationActions } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class SingleMeal extends React.Component {
+	addcart = () => {
+		var meal = this.state.Meal[0];
+		var num = this.state.num || 1;
 
-  addcart = ()=>{
-    var meal = this.state.Meal[0];
-    var num = this.state.num || 1 ;
+		AsyncStorage.getItem('cart').then(cart => {
+			AsyncStorage.setItem('cart', cart + ',' + meal.key).then(() => {
+				if (num > 1) {
+					this.setState({
+						num: num - 1
+					});
+					this.addcart();
+				} else {
+					alert('تم الاضافه الى السله');
+				}
+			});
+		});
+	};
 
-      AsyncStorage.getItem('cart').then((cart)=>{
-        AsyncStorage.setItem('cart',cart+','+meal.key).then(()=>{
-          if(num > 1){
-            this.setState({
-              num : num-1,
-            })
-            this.addcart()
-          }
-          else {
-            alert('تم الاضافه الى السله')
-          }
-
-        })
-
-      })
-
-
-
-  }
-
-  static navigationOptions = ({ navigation }) => ({
-    title:'الوجبه',
-    headerTintColor: Colors.smoothGray,
-    fontFamily:'myfont',
-  headerStyle: {
-    backgroundColor: Colors.mainColor,
-    borderBottomColor: Colors.mainColor,
-    borderBottomWidth: 3,
-  },
-  headerTitleStyle: {
-    fontWeight: '300',
-    color: '#ffffff',
-    fontFamily: 'myfont',
-    fontSize: 16
-  },
-  });
-  constructor(props) {
+	static navigationOptions = ({ navigation }) => ({
+		title: 'الوجبه',
+		headerTintColor: Colors.smoothGray,
+		fontFamily: 'myfont',
+		headerStyle: {
+			backgroundColor: Colors.mainColor,
+			borderBottomColor: Colors.mainColor,
+			borderBottomWidth: 3
+		},
+		headerTitleStyle: {
+			fontWeight: '300',
+			color: '#ffffff',
+			fontFamily: 'myfont',
+			fontSize: 16
+		}
+	});
+	constructor(props) {
 		super(props);
 
 		this.state = {
-      doneFetches:0,
-      Restaurant: [
+			doneFetches: 0,
+			Restaurant: [],
+			Meal: [],
+			num: 1
+		};
+	}
+	componentDidMount() {
+		fetch(
+			Server.dest +
+				'/api/store-info?store_id=' +
+				this.props.navigation.state.params.restaurant_id
+		)
+			.then(res => res.json())
+			.then(restaurants => {
+				fetch(
+					Server.dest +
+						'/api/product-info?product_id=' +
+						this.props.navigation.state.params.meal_id
+				)
+					.then(res => res.json())
+					.then(meals => {
+						this.setState({
+							doneFetches: 1,
+							Restaurant: [restaurants.response],
+							Meal: [meals.response]
+						});
+						console.log(this.state.Restaurant);
+					});
+			});
+	}
+	render() {
+		const { params } = this.props.navigation.state;
+		if (this.state.doneFetches == 0)
+			return <LoadingIndicator size="large" color="#B6E3C6" />;
 
+		return (
+			<View>
+				<FlatList
+					automaticallyAdjustContentInsets={false}
+					style={{
+						backgroundColor: 'white',
+						borderBottomWidth: 0.3,
+						borderBottomColor: 'black'
+					}}
+					removeClippedSubviews={false}
+					ItemSeparatorComponent={() => (
+						<View style={{ height: 5, backgroundColor: Colors.smoothGray }} />
+					)}
+					data={this.state.Restaurant}
+					renderItem={({ item }) => (
+						<RestaurantBox
+							stars={item.stars}
+							name={item.name}
+							time={item.time}
+							desc={item.desc}
+							image={item.image}
+							price={item.deliver_price}
+						/>
+					)}
+				/>
+				<FlatList
+					automaticallyAdjustContentInsets={false}
+					style={{ backgroundColor: 'white' }}
+					removeClippedSubviews={false}
+					ListFooterComponent={() => (
+						<View>
+							<View
+								style={{
+									flex: 1,
+									flexDirection: 'row',
+									borderColor: Colors.mainColor,
+									borderWidth: 1,
+									margin: 18,
+									borderRadius: 18,
+									padding: 10
+								}}
+							>
+								<View
+									style={{
+										flex: 1.5,
+										alignItems: 'center',
+										justifyContent: 'center'
+									}}
+								>
+									<Ionicons
+										onPress={() => {
+											if (this.state.num != 1) {
+												this.setState({ num: this.state.num - 1 });
+											}
+										}}
+										name="ios-remove-circle-outline"
+										size={40}
+										color={Colors.mainColor}
+									/>
+								</View>
+								<View
+									style={{
+										flex: 3,
+										alignItems: 'center',
+										justifyContent: 'center'
+									}}
+								>
+									<Text
+										style={{
+											alignItems: 'center',
+											color: Colors.mainColor,
+											fontSize: 18
+										}}
+									>
+										{this.state.num}
+									</Text>
+								</View>
 
-      ],
-			Meal: [
+								<View
+									style={{
+										flex: 1.5,
+										alignItems: 'center',
+										justifyContent: 'center'
+									}}
+								>
+									<Ionicons
+										onPress={() => this.setState({ num: this.state.num + 1 })}
+										name="ios-add-circle-outline"
+										size={40}
+										color={Colors.mainColor}
+									/>
+								</View>
+							</View>
 
-      ]
-      }
-    }
-    componentDidMount(){
-        fetch(Server.dest + '/api/store-info?store_id='+this.props.navigation.state.params.restaurant_id).then((res)=>res.json()).then((restaurants)=>{
-          fetch(Server.dest + '/api/product-info?product_id='+this.props.navigation.state.params.meal_id).then((res)=>res.json()).then((meals)=>{
-
-            this.setState({
-              doneFetches:1,
-              Restaurant: [restaurants.response],
-        			Meal: [meals.response]
-            })
-            console.log(this.state.Restaurant)
-        })
-      })
-    }
-  render() {
-const { params } = this.props.navigation.state;
-if(this.state.doneFetches == 0)
-    return (<LoadingIndicator size="large" color="#B6E3C6" />);
-
-    return (
-      <View>
-      <FlatList
-        automaticallyAdjustContentInsets={false}
-        style={{ backgroundColor: 'white',borderBottomWidth:.3,borderBottomColor:'black' }}
-        removeClippedSubviews={false}
-        ItemSeparatorComponent={ () => <View style={{ height: 5, backgroundColor: Colors.smoothGray }} /> }
-        data={this.state.Restaurant}
-        renderItem={({ item }) => (
-
-          <RestaurantBox
-
-            stars={item.stars}
-            name={item.name}
-            time={item.time}
-            desc={item.desc}
-            image={item.image}
-            price={item.deliver_price}
-          />
-        )}
-      />
-      <FlatList
-        automaticallyAdjustContentInsets={false}
-        style={{ backgroundColor: 'white' }}
-        removeClippedSubviews={false}
-        ListFooterComponent={()=>(
-          <View>
-          <KeyboardAvoidingView>
-          <TextInput
-            underlineColorAndroid="transparent"
-            placeholder="عدد الوجبات"
-            placeholderTextColor="#CCCCCC"
-            keyboardType="numeric"
-            style={{
-              flex: 0.5,
-              fontSize: 33,
-              color: Colors.mainColor,
-              textAlign: 'center',
-              fontFamily: 'myfont',
-              borderRadius: 4,
-              backgroundColor: 'transparent',
-              borderBottomColor: Colors.fadedMainColor,
-              borderBottomWidth: 1
-            }}
-            value={this.state.num}
-            onChangeText={(text) => {
-              this.setState({ num: text },()=>{
-                alert(this.state.num);
-              })
-          }}
-          />
-          <Button
-              onPress={() => {
-                  this.addcart();
-              }}
-              color='white'
-              backgroundColor={Colors.mainColor}
-              containerViewStyle={{borderRadius:15}}
-              borderRadius={15}
-              buttonStyle={{ padding: 10 }}
-              textStyle={{ fontFamily: 'myfont' }}
-              title="اضف الى السله" />
-              </KeyboardAvoidingView>
-              </View>
-
-        )
-        }
-        ItemSeparatorComponent={ () => <View style={{ height: 5, backgroundColor: Colors.smoothGray }} /> }
-        data={this.state.Meal}
-        renderItem={({ item }) => (
-
-          <MealBox
-            name={item.name}
-            time={item.time}
-            desc={item.desc}
-            image={item.image}
-            price={item.price}
-          />
-        )}
-      />
-
-      </View>
-    );
-  }
+							<Button
+								onPress={() => {
+									this.addcart();
+								}}
+								color="white"
+								backgroundColor={Colors.mainColor}
+								containerViewStyle={{ borderRadius: 15 }}
+								borderRadius={15}
+								buttonStyle={{ padding: 10 }}
+								textStyle={{ fontFamily: 'myfont' }}
+								title="اضف الى السله"
+							/>
+						</View>
+					)}
+					ItemSeparatorComponent={() => (
+						<View style={{ height: 5, backgroundColor: Colors.smoothGray }} />
+					)}
+					data={this.state.Meal}
+					renderItem={({ item }) => (
+						<MealBox
+							name={item.name}
+							time={item.time}
+							desc={item.desc}
+							image={item.image}
+							price={item.price}
+						/>
+					)}
+				/>
+			</View>
+		);
+	}
 }
