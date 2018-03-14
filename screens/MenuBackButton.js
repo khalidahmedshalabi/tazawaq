fbd9875ee225644dd95a6f903698c090ccbb0da8
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-03-14 23:40:59 +0200

    about screen button edit

--- screens/MenuBackButton.js
+++ screens/MenuBackButton.js
@@ -12,13 +12,13 @@ export default class MenuBackButton extends React.Component {
     return (
         <TouchableOpacity
             onPress={ () => this.props.navigation.goBack() }
-            style={{ paddingTop: 20, paddingLeft:5, flexDirection: 'row', alignItems: 'center' }}>
+            style={{ ios:{paddingTop: 20, paddingLeft:5}, flexDirection: 'row', alignItems: 'center' }}>
 
             <Ionicons
               name={ (Platform.OS == 'ios') ? ("ios-arrow-back"):("md-arrow-back") }
               size={28}
               color="#000"
-              style={{ backgroundColor: 'transparent', marginRight: 7}}/>
+              style={{ backgroundColor: 'transparent', marginRight: 7,marginTop:7}}/>
 
             <Text style={{ backgroundColor:'transparent', color: "#000",fontFamily:'myfont', fontWeight: 'bold', fontSize: 16 }}>رجوع</Text>
         </TouchableOpacity>
